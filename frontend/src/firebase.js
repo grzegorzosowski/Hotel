6b16e3654909dede6bdff914c8ab9e2995d4cef4
Grzegorz <osowskigrzegorz95@gmail.com>
2023-03-07 16:20:43 +0100

    Firebase: builded React app deployed, front is workingon firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,13 @@
+ 
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCwV0h9JF0IjZgc-dmkX7hOK5sEeTLWSyU",
+  authDomain: "my-new-app-f64ef.firebaseapp.com",
+  projectId: "my-new-app-f64ef",
+  storageBucket: "my-new-app-f64ef.appspot.com",
+  messagingSenderId: "1047886440315",
+  appId: "1:1047886440315:web:4773332b48cfb9f65ebfde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
